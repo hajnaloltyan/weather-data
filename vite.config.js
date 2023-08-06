eb1f61fb59b7f953ad65a166a29517957311bd43
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-08-06 22:49:55 +0200

    Setup project with linters installed

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/weather-data/',
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+    css: true,
+  },
+});
