89da515fe89089f42a7aebb6a07f21529f7d6414
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-08-06 23:03:21 +0200

    Create Humidity component with line chart to showcase humidity data

---
+++ src/components/Humidity/Humidity.jsx
@@ -0,0 +1,26 @@
+import { Line } from 'react-chartjs-2';
+import { Chart } from 'chart.js/auto';
+import { useSelector } from 'react-redux';
+import { getHumidities, getDates } from '../../redux/weatherData/weatherDataSlice';
+import './Humidity.css';
+
+const Humidity = () => {
+  const humidities = useSelector(getHumidities);
+  const dates = useSelector(getDates);
+
+  const humidityData = {
+    labels: dates,
+    datasets: [
+      {
+        label: 'Humidity (%)',
+        data: humidities,
+        backgroundColor: 'green',
+        fill: 'start',
+      },
+    ],
+  };
+
+  return <Line data={humidityData} className="humidity" />;
+};
+
+export default Humidity;
