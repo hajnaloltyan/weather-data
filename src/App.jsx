9bb08bd5dd531d9cddcca56d709fdee0cee69588
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-08-06 22:53:41 +0200

    Setup store, add routes

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layouts/Layout';
+import DefaultLocations from './components/DefaultLocations/DefaultLocations';
+import WeatherCharts from './components/WeatherCharts/WeatherCharts';
+import WeatherDetails from './components/WeatherDetails/WeatherDetails';
+import WeatherDataTable from './components/WeatherDataTable/WeatherDataTable';
+import Temperatures from './components/Temperatures/Temperatures';
+import Precipitation from './components/Precipitation/Precipitation';
+import Humidity from './components/Humidity/Humidity';
+import DetailsLayout from './components/Layouts/DeatilsLayout';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<DefaultLocations />} />
+        <Route path="details" element={<DetailsLayout />}>
+          <Route path="all-details" element={<WeatherDetails />} />
+          <Route path="table" element={<WeatherDataTable />} />
+          <Route path="all-charts" element={<WeatherCharts />} />
+          <Route path="temperatures" element={<Temperatures />} />
+          <Route path="precipitation" element={<Precipitation />} />
+          <Route path="humidity" element={<Humidity />} />
+        </Route>
+      </Route>
+    </Routes>
+  </Router>
+);
+
+export default App;
