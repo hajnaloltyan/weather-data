68901657550c0e5f868ea85ca92b5a7848bc33dd
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-08-06 22:55:44 +0200

    Create the search bar and header part of the page

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,85 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, NavLink } from 'react-router-dom';
+import { fetchPlaceNames, AllSearches, setLocation } from '../../redux/search/searchSlice';
+import './Search.css';
+import { fetchWeatherData } from '../../redux/weatherData/weatherDataSlice';
+
+const Search = () => {
+  const [query, setQuery] = useState('');
+  const dispatch = useDispatch();
+
+  const placeNames = useSelector(AllSearches);
+  useEffect(() => {
+    if (query.trim() !== '' && query.length > 2) {
+      dispatch(fetchPlaceNames(query));
+    }
+  }, [dispatch, query]);
+
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value;
+    setQuery(inputValue);
+  };
+
+  const handleSelectLocation = (name, lat, lon) => {
+    dispatch(fetchWeatherData({ lat, lon }));
+    setQuery('');
+    dispatch(setLocation(name));
+  };
+
+  const renderPlaceNames = (places) => {
+    if (places.length === 0 || query.trim() === '') {
+      return null;
+    }
+
+    return (
+      <ul className="searchResults">
+        {places.map(({
+          id, lat, lon, name, state, country,
+        }) => (
+          <li key={id}>
+            <Link to="details/all-details">
+              <button
+                type="button"
+                className="searchItem"
+                onClick={() => handleSelectLocation(name, lat, lon)}
+              >
+                {name}
+                ,
+                {' '}
+                {state}
+                ,
+                {' '}
+                {country}
+              </button>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  return (
+    <div className="header-border">
+      <div className="wrapper">
+        <h1 className="pageTitle">Weather Data</h1>
+        <nav className="home">
+          <NavLink to="/">Home</NavLink>
+        </nav>
+      </div>
+      <div className="searchContainer">
+        <p>Please select a location:</p>
+        <input
+          type="text"
+          value={query}
+          onChange={handleInputChange}
+          placeholder="Search for a place..."
+          className="searchInput"
+        />
+        {renderPlaceNames(placeNames)}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
